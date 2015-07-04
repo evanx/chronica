6036340275ef712a5c5479029adceb110dc08726
Evan Summers <evans@saratoga.co.za>
2015-07-04 12:55:39 +0200

    scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 
-
 if ! ls -l  ~/.chronica-active.yaml
 then
   echo 'Create config file e.g.  ~/.chronica-active.yaml'
@@ -10,6 +9,27 @@ fi
 if ! cat index.js | grep -q evanxsummers
 then
   echo 'Run from chronica directory e.g. cd ~/chronica-active'
+  exit 1
+fi
+
+if ! pwd | grep -q "/chronica-active"
+then
+  echo "Please run from chronica-active directory"
+  exit 1
+fi
+
+[ -d util/.git ] || git submodule update
+[ -f util/Utils.js ] || git submodule update
+
+if [ ! -d util/.git ]
+then
+   echo 'Please init the util/ git submodule: git submodule init'
+   exit 1
+fi
+
+if [ ! -f util/Utils.js ]
+then
+  echo 'Please update the util/ git submodule: git submodule update'
   exit 1
 fi
 
