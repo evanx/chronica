476693d29a44a29680a2e07c0d5fc55f919bc49c
Evan Summers <evans@saratoga.co.za>
2015-07-05 19:42:59 +0200

    update

--- test/adhoc/promises/throwing.js
+++ test/adhoc/promises/throwing.js
@@ -11,43 +11,57 @@ git clone https://github.com/evanx/chronica.git &&
 import assert from 'assert';
 import lodash from 'lodash';
 
+/*
+The test harness checks the prefix of the test's key i.e. function name.
+It checks accordingly that the function:
+- returns its key
+- throws its key
+- swallows the exception
+*/
 const tests = {
-   returnSanity(key) { // sanity check
+   returnsSanity(key) { // sanity check
       return key;
    },
-   async returnSanityAsync(key) { // sanity check
+   async returnsSanityAsync(key) { // sanity check
       return key;
    },
-   async returnPromiseAsync(key) { // sanity check
+   async returnsPromiseAsync(key) { // sanity check
       return Promise.resolve(key);
    },
-   async returnAwaitAsync(key) { // sanity check
+   async returnsAwaitAsync(key) { // sanity check
       return await Promise.resolve(key);
    },
-   throwSanity(key) { // sanity check
+   throwsSanity(key) { // sanity check
       throw key;
    },
-   async throwSanityAsync(key) { // sanity check
+   async throwsSanityAsync(key) { // sanity check
       throw key;
    },
-   async throwSanityPromiseAsync(key) { // sanity check
+   async throwsSanityPromiseAsync(key) { // sanity check
       return Promise.reject(key);
    },
-   swallow(key) { // programmer beware
-      // any sync method will swallow errors in promises
+   async throwsSanityAwaitAsync(key) { // sanity check
+      return await Promise.reject(key);
+   },
+   swallows(key) { // programmer beware
+      // any sync function swallows errors in promises
       // so you must have catch() if not returning the promise
       Promise.resolve('any').then(value => {throw value});
    },
-   swallowBetter(key) { // better programming
+   swallowsBetter(key) { // better programming
       Promise.resolve('any').then(value => {throw value})
-      .catch(err => assert.equals(err, 'any'));
+      .catch(err => {
+         if (err !== 'any') {
+            console.error('ERROR:', key, err);
+         }
+      });
    },
-   swallowCatchThrow(key) { // programmer beware
-      // any sync method will swallow errors in catch
+   swallowsCatchThrow(key) { // programmer beware
+      // any sync function swallows errors in catch
       Promise.resolve('any').then(value => {throw value})
       .catch(err => {throw err});
    },
-   async swallowAsync(key) { // programmer beware
+   async swallowsAsync(key) { // programmer beware
       // swallows errors in promises not awaited or returned
       try {
          Promise.resolve('any').then(value => {throw value});
@@ -55,14 +69,14 @@ const tests = {
          assert(false, 'we cannot catch errors without await');
       }
    },
-   async throwAsync(key) { // good usage: return promise
+   async throwsAsync(key) { // good usage: return promise
       try {
         return Promise.resolve(key).then(value => {throw value});
      } catch (e) {
         assert(false, 'we cannot catch errors without await');
      }
    },
-   async throwAwaitAsync(key) { // best usage: await promise and catch errors
+   async throwsAwaitAsync(key) { // best usage: await promise and catch errors
       try {
          await Promise.resolve(key).then(value => {throw value});
          assert(false, 'error ocurred above, caught below');
@@ -78,25 +92,25 @@ async function run(keys) {
       try {
          if (/Async/.test(key)) {
             let returned = await tests[key](key);
-            if (/return/.test(key)) {
+            if (/^returns/.test(key)) {
                assert.equal(returned, key, 'returned: ' + key);
             }
             console.log('done await', key);
          } else {
             let returned = tests[key](key);
-            if (/return/.test(key)) {
+            if (/^returns/.test(key)) {
                console.log('returned sync', key, returned);
                assert.equal(returned, key, 'returned: ' + key);
             }
             console.log('done sync', key);
          }
-         assert(!/throw/.test(key), 'not thrown: ' + key);
+         assert(!/^throws/.test(key), 'not thrown: ' + key);
       } catch (e) {
          console.log('catch', key, e);
          if (e.stack) {
             console.error(e.stack); // show programming errors
          }
-         if (/throw/.test(key)) {
+         if (/^throws/.test(key)) {
             assert.equal(e, key);
          } else {
             console.error('ERROR: ' + e);
@@ -112,8 +126,12 @@ var keys = Object.keys(tests);
 run(keys).then(results => {
    assert(results.length, keys.length, 'results count matches tests');
    console.info('then', results.length, keys.length);
-   results.forEach((result, index) => {console.log(result, keys[index])});
-   console.info('OK');
+   let invalidResults = results.filter((result, index) => result !== keys[index]);
+   if (invalidResults.length === 0) {
+      console.info('OK');
+   } else {
+      console.info('INVALID:', invalidResults);
+   }
 }, reason => {
    console.error('run rejected:', reason);
 }).catch(error => {
@@ -122,55 +140,49 @@ run(keys).then(results => {
 
 /* outputs
 evans@boromir:~/chronica$ babel-node --stage 0 test/adhoc/promises/throwing.js
-start returnSanity
-returned sync returnSanity returnSanity
-done sync returnSanity
-end returnSanity
-start returnSanityAsync
-start returnPromiseAsync
-start throwSanity
-catch throwSanity throwSanity
-end throwSanity
-start throwSanityAsync
-start throwSanityPromiseAsync
-start swallow
-done sync swallow
-end swallow
-start swallowBetter
-done sync swallowBetter
-end swallowBetter
-start swallowCatchThrow
-done sync swallowCatchThrow
-end swallowCatchThrow
-start swallowAsync
-start throwAsync
-start throwAwaitAsync
-done await returnSanityAsync
-end returnSanityAsync
-catch throwSanityAsync throwSanityAsync
-end throwSanityAsync
-done await swallowAsync
-end swallowAsync
-done await returnPromiseAsync
-end returnPromiseAsync
-catch throwSanityPromiseAsync throwSanityPromiseAsync
-end throwSanityPromiseAsync
-catch throwAsync throwAsync
-end throwAsync
-catch throwAwaitAsync throwAwaitAsync
-end throwAwaitAsync
-then 12 12
-returnSanity returnSanity
-returnSanityAsync returnSanityAsync
-returnPromiseAsync returnPromiseAsync
-throwSanity throwSanity
-throwSanityAsync throwSanityAsync
-throwSanityPromiseAsync throwSanityPromiseAsync
-swallow swallow
-swallowBetter swallowBetter
-swallowCatchThrow swallowCatchThrow
-swallowAsync swallowAsync
-throwAsync throwAsync
-throwAwaitAsync throwAwaitAsync
+start returnsSanity
+returned sync returnsSanity returnsSanity
+done sync returnsSanity
+end returnsSanity
+start returnsSanityAsync
+start returnsPromiseAsync
+start returnsAwaitAsync
+start throwsSanity
+catch throwsSanity throwsSanity
+end throwsSanity
+start throwsSanityAsync
+start throwsSanityPromiseAsync
+start throwsSanityAwaitAsync
+start swallows
+done sync swallows
+end swallows
+start swallowsBetter
+done sync swallowsBetter
+end swallowsBetter
+start swallowsCatchThrow
+done sync swallowsCatchThrow
+end swallowsCatchThrow
+start swallowsAsync
+start throwsAsync
+start throwsAwaitAsync
+done await returnsSanityAsync
+end returnsSanityAsync
+catch throwsSanityAsync throwsSanityAsync
+end throwsSanityAsync
+done await swallowsAsync
+end swallowsAsync
+done await returnsPromiseAsync
+end returnsPromiseAsync
+done await returnsAwaitAsync
+end returnsAwaitAsync
+catch throwsSanityPromiseAsync throwsSanityPromiseAsync
+end throwsSanityPromiseAsync
+catch throwsSanityAwaitAsync throwsSanityAwaitAsync
+end throwsSanityAwaitAsync
+catch throwsAsync throwsAsync
+end throwsAsync
+catch throwsAwaitAsync throwsAwaitAsync
+end throwsAwaitAsync
+then 14 14
 OK
 */
