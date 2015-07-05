d00ab2f94970c4c69f640ccab5aa81daca5c8a92
Evan Summers <evans@saratoga.co.za>
2015-07-05 21:22:21 +0200

    update

--- test/adhoc/promises/throwing.js
+++ test/adhoc/promises/throwing.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
 // ISC license, see http://github.com/evanx/redex/LICENSE
 
+// Notes: https://github.com/evanx/chronica/tree/master/test/adhoc/promises
+
 import assert from 'assert';
 import lodash from 'lodash';
 
@@ -125,52 +127,4 @@ async function run(keys) {
    });
 }
 
-/* Notes:
-
-Source: https://github.com/evanx/chronica/tree/master/test/adhoc/promises
-
-<!---
-Async functions are an ES7 proposal (stage 0).
-
-The individual test function names are keys from Object.keys(tests)
-
-The test harness checks the suffix and prefix of the test's key e.g. 'returnsPromiseAync'
-
-The suffix is 'Async' if it is an ES7 async function.
-
-The prefix is 'returns' or 'throws.'
-We check accordingly that the function:
-- returns its key, or
-- throws its key
-where we pass the test its key as an argument.
-
-Any test that throws or returns any other object is considered a failure.
-
-Note that the returned promises are await'ed, and so are converted into their resolved values by ES7.
-
-Note that we throw an error is our promise resolved function to simulate an error therein.
-
-### Running
-
-We run this test as follows:
-
-git clone https://github.com/evanx/chronica.git &&
-  cd ~/chronica &&
-  npm install &&
-  node_modules/babel/bin/babel-node --stage 0 test/adhoc/promises/throwing.js
-
-#### Output
-
-evans@boromir:~/chronica$ babel-node --stage 0 test/adhoc/promises/throwing.js | tail
-end throwsSanityPromiseAsync
-catch throwsSanityAwaitAsync throwsSanityAwaitAsync
-end throwsSanityAwaitAsync
-caught throwsAwaitAsync throwsAwaitAsync
-catch throwsAsync throwsAsync
-end throwsAsync
-catch throwsAwaitAsync throwsAwaitAsync
-end throwsAwaitAsync
-results: 14 returnsSanity throwsAwaitAsync
-OK
---->
-*/
+// Notes: https://github.com/evanx/chronica/tree/master/test/adhoc/promises
