4dbc7b868b7b8cf12d4fe434315a64fc7a3c20d1
Evan Summers <evans@saratoga.co.za>
2015-08-23 03:34:24 +0200

    test

--- index.js
+++ index.js
@@ -3,4 +3,6 @@
 
 require('babel/register')({stage: 0});
 Object.assign(global, require('./util/Utils'));
+global.Redis = require('./util/Redis');
+global.YamlFiles = require('./util/YamlFiles');
 require('./lib/ApplicationConfigurator');
