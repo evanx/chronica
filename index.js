55e47eaf4b245a134bc32ad2e3cb15204e9c0e41
Evan Summers <evans@saratoga.co.za>
2015-08-23 22:10:13 +0200

    readme

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
 // ISC license, see http://github.com/evanx/redex/LICENSE
 
+if (process.argv.length > 3 && process.argv[3] === 'debug') {
+   global.loggerLevel = 'debug';
+   console.log('loggerLevel', global.loggerLevel, process.env.loggerLevel);
+}
 require('babel/register')({stage: 0});
 Object.assign(global, require('./util/Utils'));
 global.Redis = require('./util/Redis');
