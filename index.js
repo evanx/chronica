b505a8dfb76c666424e4941965eeaed2b08368fa
Evan Summers <evan.summers@gmail.com>
2016-03-06 15:05:00 +0200

    changed:

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ if (process.argv.length > 3 && process.argv[3] === 'debug') {
    console.log('loggerLevel', global.loggerLevel, process.env.loggerLevel);
 }
 require('babel/register')({stage: 0});
-Object.assign(global, require('./util/Utils'));
+global.Loggers = require('./util/Loggers');
+Object.assign(global, require('./util/Global'));
 global.Files = require('./util/Files');
 global.Redis = require('./util/Redis');
 global.YamlFiles = require('./util/YamlFiles');
